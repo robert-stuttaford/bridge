b0767aa9c68ca03e91957ff52c6d5bb18ce26fde
Robert Stuttaford <robert@cognician.com>
2018-02-18 12:44:13 +0200

    Tweak

--- src/bridge/data/datomic.clj
+++ src/bridge/data/datomic.clj
@@ -2,13 +2,12 @@
   (:require [datomic.api :as d]
             [integrant.core :as ig]))
 
+(defmethod ig/init-key :datomic/connection [_ {:keys [uri]}]
+  (d/create-database uri)
+  (d/connect uri))
+
 (defn wrap-datomic [handler conn]
   (fn [request]
     (handler (merge request
                     {:datomic/conn conn
                      :datomic/db (d/db conn)}))))
-
-(defmethod ig/init-key :datomic/connection [_ {:keys [uri]}]
-  (d/create-database uri)
-  (d/connect uri))
-
