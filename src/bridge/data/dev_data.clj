51d732a645e10d5d64d50ab648104f3c8f7f8541
Robert Stuttaford <robert@cognician.com>
2018-03-17 18:26:02 +0200

    Use person.data APIs in dev-data

--- src/bridge/data/dev_data.clj
+++ src/bridge/data/dev_data.clj
@@ -3,16 +3,23 @@
             [bridge.person.data :as person.data]
             [integrant.core :as ig]))
 
+(defn add-person! [conn {:person/keys [email] :as new-person}]
+  (when (nil? (person.data/person-id-by-email (datomic/db conn) email))
+    (let [new-person-tx (person.data/new-person-tx new-person
+                                                   #:person{:name     "Test Name"
+                                                            :email    "test@cb.org"
+                                                            :password "secret"})]
+      (person.data/save-new-person! conn new-person-tx)
+      (person.data/confirm-email! conn
+                                  (person.data/person-id-by-email (datomic/db conn) email)
+                                  (:person/email-confirm-token new-person-tx)))))
+
 (defmethod ig/init-key :datomic/dev-data [_ {{:datomic/keys [mode conn]} :datomic}]
   (datomic/with-datomic-mode mode
     ;; schema
     (datomic/transact! conn person.data/schema)
 
     ;; test people
-    (when (nil? (datomic/entid (datomic/db conn)
-                               [:person/email "test@cb.org"]))
-      (->> [(person.data/new-person-tx
-             #:person{:name     "Test Name"
-                      :email    "test@cb.org"
-                      :password "secret"})]
-           (datomic/transact! conn)))))
+    (add-person! conn #:person{:name     "Test Name"
+                               :email    "test@cb.org"
+                               :password "secret"})))
