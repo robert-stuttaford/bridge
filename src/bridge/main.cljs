7ba6c86a4cc9506dd44d380d21dc11e413669f07
Robert Stuttaford <robert@cognician.com>
2018-03-19 12:22:52 +0200

    Add a navbar to client
    Include signed in person's name in navbar

--- src/bridge/main.cljs
+++ src/bridge/main.cljs
@@ -1,12 +1,35 @@
 (ns bridge.main
-  (:require [rum.core :as rum]))
+  (:require [cljs.reader :as edn]
+            [rum.core :as rum]))
 
 (enable-console-print!)
 
-(rum/defc app []
-  [:div "Hello"
-   [:br]
-   [:a {:href "/logout"} "Logout"]])
+(defn read-edn-from-script-tag [element]
+  (some-> element
+          .-textContent
+          edn/read-string))
+
+(rum/defc navbar [state]
+  [:nav.navbar.is-fixed-top.is-info
+   [:.container
+    [:.navbar-brand
+     [:a.navbar-item {:href "/"}
+      [:img {:src (str "http://www.clojurebridge.org/assets/"
+                       "cb-logo-cbba8a3667c88b78189d8826867cf01a.png")}]]]
+    [:.navbar-menu
+     [:.navbar-start
+      [:a.navbar-item {:href "javascript:"} "Browse Events"]
+      [:a.navbar-item {:href "javascript:"} "Add New Event"]]
+     [:.navbar-end
+      [:.navbar-item (get-in state [:person :person/name])]
+      [:a.navbar-item {:href "/logout"} "Sign Out"]]]]])
+
+(rum/defc app [initial-data]
+  [:div
+   (navbar initial-data)
+   [:.container {:style {:margin-top "50px"}}]])
 
 (defn ^:export refresh []
-  (rum/mount (app) (js/document.getElementById "mount")))
+  (rum/mount (app (some-> (js/document.getElementById "initial-data")
+                          read-edn-from-script-tag))
+             (js/document.getElementById "mount")))
--- src/bridge/web/client.clj
+++ src/bridge/web/client.clj
@@ -1,16 +1,25 @@
 (ns bridge.web.client
-  (:require [bridge.web.template :as web.template]
+  (:require [bridge.data.datomic :as datomic]
+            [bridge.web.template :as web.template]
             [buddy.auth :as buddy]
             [clojure.string :as str]
             [ring.util.response :as response]))
 
-(defn client [req]
-  (if-not (buddy/authenticated? req)
+(defn edn-script-tag [id data]
+  [:script
+   {:id   id
+    :type "application/edn"
+    :dangerouslySetInnerHTML {:__html (pr-str data)}}])
+
+(defn client [{:datomic/keys [db]
+               {:keys [identity]} :session
+               :as request}]
+  (if-not (buddy/authenticated? request)
     (buddy/throw-unauthorized)
     (web.template/hiccup-response
      [:div#mount]
-     [:script {:type "application/edn"}
-      (pr-str (:session req))]
+     (edn-script-tag "initial-data"
+                     {:person (datomic/pull db [:person/name] identity)})
      [:script {:src "/js/app.js"}]
      [:script "bridge.main.refresh();"])))
 
