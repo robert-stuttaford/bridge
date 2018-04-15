cc83b8566d39c0cd72085cf84af8eab20df8f24f
Robert Stuttaford <robert@cognician.com>
2018-04-15 11:32:58 +0200

    Grid for events list

--- src/bridge/event/ui/list.cljs
+++ src/bridge/event/ui/list.cljs
@@ -6,27 +6,29 @@
             [bridge.ui.util :refer [<== ==>]]))
 
 (defn list-events []
-  [:div.column.is-two-fifths
+  [:div
    [:h3.title.is-4.spaced "All Events"]
    [:a.button {:href     "javascript:"
                :on-click #(==> (ui.base/load-on-view {:view :list-events}))}
     "Refresh"]
 
-   (for [{:event/keys [title slug status start-date end-date
-                       registration-close-date]}
-         (vals (<== [:bridge.event.ui/events]))]
+   (for [events (partition-all 3 (vals (<== [:bridge.event.ui/events])))]
+     [:div.columns {:key (apply str (map :event/slug events))}
+      (for [{:event/keys [title slug status start-date end-date
+                          registration-close-date]}
+            events]
 
-     [:div.card {:key   slug
-                 :style {:margin-top "2rem"}}
-      [:header.card-header
-       [:p.card-header-title title " (" (data.string/keyword->label status) ")"]]
-      [:div.card-content
-       [:div.content
-        [:strong [data.date/date-time start-date]] " - "
-        [:strong [data.date/date-time end-date]]
-        [:br]
-        "Registration ends "
-        [:strong [data.date/date-time registration-close-date]]]]
-      [:footer.card-footer
-       [:a.card-footer-item (ui.routes/turbo-links (str "/app/events/edit/" slug))
-        "Edit"]]])])
+        [:div.column.is-one-third {:key slug}
+         [:div.card {:style {:margin-top "2rem"}}
+          [:header.card-header
+           [:p.card-header-title title " (" (data.string/keyword->label status) ")"]]
+          [:div.card-content
+           [:div.content
+            [:strong [data.date/date-time start-date]] " - "
+            [:strong [data.date/date-time end-date]]
+            [:br]
+            "Registration ends "
+            [:strong [data.date/date-time registration-close-date]]]]
+          [:footer.card-footer
+           [:a.card-footer-item (ui.routes/turbo-links (str "/app/events/edit/" slug))
+            "Edit"]]]])])])
